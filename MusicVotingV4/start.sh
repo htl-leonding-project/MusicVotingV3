484d5d50590407826158e801fea18ae674fc79e1
AnnaHartl <anna.sofie.hartl@gmail.com>
2022-06-30 13:36:07 +0200

    changed shell script

--- MusicVotingV4/start.sh
+++ MusicVotingV4/start.sh
@@ -4,6 +4,13 @@ export IP_ADDRESS=$(ip -4 addr show wlp0s20f3 | grep -oP '(?<=inet\s)\d+(\.\d+){
 
 echo $IP_ADDRESS
 
+sed -r -i 's/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b'/"$IP_ADDRESS"/ addMusic/src/environments/environment.ts 
+
+
+sed -r -i 's/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b'/"$IP_ADDRESS"/ showMusic/src/environments/environment.ts 
+
+
+
 export QUARKUS_HTTP_HOST=$IP_ADDRESS
 
 gnome-terminal -- sh -c "./database.sh"
