0abef438681e6521ce76841186783e760be49aff
AnnaHartl <anna.sofie.hartl@gmail.com>
2023-04-18 16:41:03 +0200

    changed base url bauer

--- addMusic/src/environments/environment.prod.ts
+++ addMusic/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   timeOutAtAdd: 3000,
-  host: "https://student.cloud.htl-leonding.ac.at/t.stuetz/music-voting/api/",
+  host: "https://student.cloud.htl-leonding.ac.at/p.bauer/music-voting/api/",
   home:"home"
 };
