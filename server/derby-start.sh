319c77553e844d7f97aedf9b953612ab9c9c148d
FroellerFelix <felix.froeller@gmail.com>
2024-06-25 09:56:09 +0200

    attempted fix: removed deleting ./db folder in derby-start.sh

--- server/derby-start.sh
+++ server/derby-start.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# Check if the directory ./db exists
-if [ -d "./db" ]; then
-  # If it exists, delete the directory
-  rm -rf ./db
-fi
-
-# Continue with the rest of your script
 cd db
 ./db-derby-10.15.2.0-bin/bin/startNetworkServer -noSecurityManager
