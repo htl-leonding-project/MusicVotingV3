dd7bbae19e368c8040b2157660d7dd1e162b47a6
Linus-69420 <Linusnestler@gmail.com>
2024-06-26 12:16:48 +0200

    fix: right url for qr code

--- addMusic/src/app/qr-code/qr-code.component.ts
+++ addMusic/src/app/qr-code/qr-code.component.ts
@@ -26,7 +26,7 @@ export class QrCodeComponent implements OnInit {
     dialogConfig.disableClose = true
     let dialogref=this.matDialog.open(DialogBodyComponent, dialogConfig)
     dialogref.afterClosed().subscribe(result => {
-      this.qrCodeUrl = environment.host+environment.home
+      this.qrCodeUrl = environment.host.replace(":8080", "")+environment.home
       console.log(this.qrCodeUrl, "sollte nich localhost sein amk")
     });
 
